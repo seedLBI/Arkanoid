b30863f98f3bac0385929ac027168fac8d436caf
Alexander Korzhunov <sosn228300@gmail.com>
2025-12-14 13:16:14 +0700

    Кило зависимостей рядом проектом...

--- source/Game/Math/Triangle/Triangle.cpp
+++ source/Game/Math/Triangle/Triangle.cpp
@@ -3,7 +3,7 @@
 #include "Core/CoordinateSystem/CoordinateSystem.h"
 
 void Triangle::DrawLine(QuadInstanced& renderer) {
-	renderer.AddLine(p1, p2, glm::vec4(0.f, 1.f, 0.f, 1.f), TranslateGlobalToScreen);
-	renderer.AddLine(p2, p3, glm::vec4(0.f, 1.f, 0.f, 1.f), TranslateGlobalToScreen);
-	renderer.AddLine(p3, p1, glm::vec4(0.f, 1.f, 0.f, 1.f), TranslateGlobalToScreen);
+	renderer.AddLine(p1, p2, glm::vec4(1.f, 0.2f, 1.f, 0.1f), TranslateGlobalToScreen);
+	renderer.AddLine(p2, p3, glm::vec4(1.f, 0.2f, 1.f, 0.1f), TranslateGlobalToScreen);
+	renderer.AddLine(p3, p1, glm::vec4(1.f, 0.2f, 1.f, 0.1f), TranslateGlobalToScreen);
 }
