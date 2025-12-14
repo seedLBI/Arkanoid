35a830ef1374917574c92671183915d0335138ba
Alexander Korzhunov <sosn228300@gmail.com>
2025-12-14 13:21:12 +0700

    неправильно merge сделали чтоли не доехали все файлы

--- source/Game/Math/Triangle/Triangle.cpp
+++ source/Game/Math/Triangle/Triangle.cpp
@@ -6,4 +6,5 @@ void Triangle::DrawLine(QuadInstanced& renderer) {
 	renderer.AddLine(p1, p2, glm::vec4(1.f, 0.2f, 1.f, 0.1f), TranslateGlobalToScreen);
 	renderer.AddLine(p2, p3, glm::vec4(1.f, 0.2f, 1.f, 0.1f), TranslateGlobalToScreen);
 	renderer.AddLine(p3, p1, glm::vec4(1.f, 0.2f, 1.f, 0.1f), TranslateGlobalToScreen);
+
 }
